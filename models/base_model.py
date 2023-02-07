2fec8954cabfec31884bffaf7da631ac43d5504c
Daniel Ochoja <danielochoja@yahoo.com>
2023-02-07 22:44:03 +0000

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -6,4 +6,20 @@ class BaseModel:
     def __init__(self, id, created_at, updated_at):
         """Constructor"""
         self.id = str(uuid.uuid4())
-        self.created_at
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def save(self):
+        """updates the public instance attribute updated_at
+        with the current datetime"""
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary of all key/values
+        of __dict__ of the instance"""
+        dict_values = self.__dict__
+        dict_values["__class__"] = type(self).__name__
+
+    def __str__(self):
+        """Runs when user prints instance eg. print(self)"""
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
